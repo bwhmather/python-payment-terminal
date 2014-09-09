634040e191c1dedc4c6a1b5d46361bc751828049
Ben Mather <bwhmather@bwhmather.com>
2014-09-09 12:49:29 +0100

    tests for packing and unpacking DISPLAY TEXT messages

--- nm_payment/tests/test_bbs.py
+++ nm_payment/tests/test_bbs.py
@@ -2,7 +2,7 @@ import io
 import unittest
 import threading
 
-from nm_payment.drivers.bbs import read_frame, BBSMsgRouterTerminal
+from nm_payment.drivers.bbs import read_frame, BBSMsgRouterTerminal, messages
 
 
 class TestBBS(unittest.TestCase):
@@ -36,3 +36,35 @@ class TestBBS(unittest.TestCase):
 
         terminal = BBSMsgRouterTerminal(CloseableFile())
         terminal.shutdown()
+
+    def test_pack_display_text(self):
+        self.assertEqual(
+            b'\x41000Hello World',
+            messages.pack_display_text("Hello World")
+        )
+
+        self.assertEqual(
+            b'\x41100Prompt customer',
+            messages.pack_display_text("Prompt customer", prompt_customer=True)
+        )
+
+        self.assertEqual(
+            b'\x41010Expects input',
+            messages.pack_display_text("Expects input", expects_input=True)
+        )
+
+    def test_unpack_display_text(self):
+        message = messages.unpack_display_text(b'\x41000Hello World')
+        self.assertFalse(message.prompt_customer)
+        self.assertFalse(message.expects_input)
+        self.assertEqual(message.text, "Hello World")
+
+        message = messages.unpack_display_text(b'\x41100Prompt customer')
+        self.assertTrue(message.prompt_customer)
+        self.assertFalse(message.expects_input)
+        self.assertEqual(message.text, "Prompt customer")
+
+        message = messages.unpack_display_text(b'\x41010Expects input')
+        self.assertFalse(message.prompt_customer)
+        self.assertTrue(message.expects_input)
+        self.assertEqual(message.text, "Expects input")
