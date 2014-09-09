b28170ae267da3ec6e18baad5fa4bb996681af26
Ben Mather <bwhmather@bwhmather.com>
2014-09-09 16:32:55 +0100

    split tests for messages packing and unpacking into new module

--- nm_payment/tests/__init__.py
+++ nm_payment/tests/__init__.py
@@ -1,11 +1,13 @@
 import unittest
 
 from nm_payment.tests.test_bbs import TestBBS
+from nm_payment.tests.test_bbs_messages import TestBBSMessages
 from nm_payment.tests.test_stream import TestStream
 
 
 loader = unittest.TestLoader()
 suite = unittest.TestSuite((
     loader.loadTestsFromTestCase(TestBBS),
+    loader.loadTestsFromTestCase(TestBBSMessages),
     loader.loadTestsFromTestCase(TestStream),
 ))
