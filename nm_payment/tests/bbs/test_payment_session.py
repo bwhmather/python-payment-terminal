54ad390ae998ff4a2ef585c4f857d79a21446460
Ben Mather <bwhmather@bwhmather.com>
2014-10-05 20:41:06 +0100

    factor out generally useful test code

--- nm_payment/tests/bbs/test_payment_session.py
+++ nm_payment/tests/bbs/test_payment_session.py
@@ -4,22 +4,35 @@ import unittest
 from nm_payment.drivers.bbs import _BBSPaymentSession
 
 
+def fulfilled_future(result=None):
+    """ Shortcut to create a future with its result already set
+    """
+    f = Future()
+    f.set_result(result)
+    return f
+
+
+class TerminalMockBase(object):
+    def __init__(self, test):
+        self.test = test
+        self.state = 'bank'
+
+    def _set_current_session(self, session):
+        pass
+
+    def state_change(self, initial, final):
+        self.test.assertEqual(self.state, initial)
+        self.state = final
+
+
 class TestBBSPaymentSession(unittest.TestCase):
     def test_normal(self):
-        class TerminalMock(object):
-            _state = 'bank'
+        class TerminalMock(TerminalMockBase):
+            def request_transfer_amount(self, amount):
+                self.state_change('bank', 'local')
+                return fulfilled_future()
 
-            @classmethod
-            def _set_current_session(self, session):
-                pass
-
-            @classmethod
-            def request_transfer_amount(cls, amount):
-                self.assertEqual(cls._state, 'bank')
-                cls._state = 'local'
-                f = Future()
-                f.set_result(None)
-                return f
+        terminal = TerminalMock(self)
 
         def commit_callback(result):
             nonlocal commit_callback_called
@@ -27,8 +40,9 @@ class TestBBSPaymentSession(unittest.TestCase):
             return True
         commit_callback_called = False
 
-        s = _BBSPaymentSession(TerminalMock, 10, commit_callback)
+        s = _BBSPaymentSession(terminal, 10, commit_callback)
 
+        self.assertEqual(terminal.state, 'local')
         s.on_req_local_mode('success')
 
         self.assertTrue(commit_callback_called)
