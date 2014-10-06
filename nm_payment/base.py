3a849e9ff06ad234dcbcecb6f23c5ed993e582c7
Ben Mather <bwhmather@bwhmather.com>
2014-10-06 21:33:12 +0100

    documentation

--- nm_payment/base.py
+++ nm_payment/base.py
@@ -25,6 +25,25 @@ class Terminal(object):
             self, amount, *, before_commit=None,
             on_print=None, on_display=None):
         """
+        :param amount:
+            The amount of money to request
+
+        :param before_commit:
+            function to be called after transaction is authorized but before it
+            is committed.  It should take a ``Payment`` result object as its
+            only argument.  If it returns True, the card will be charged.  If
+            it returns False the transaction will be rolled back.  Exceptions
+            will be re-raised by the ``PaymentSession.result`` method.
+
+        :param on_print:
+            function to be called when a print message is received
+
+        :param on_display:
+            function to be called when a display message is received.
+            Should accept the string to display as it's first argument and two
+            keyword arguments: ``prompt_customer`` and ``expects_input``.
+            TODO
+
         :returns: a new active ``PaymentSession`` object
         """
         pass
