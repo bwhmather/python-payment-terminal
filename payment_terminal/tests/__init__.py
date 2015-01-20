ddb8bf95c5a5a4883b0204ca206737596ac1952e
Ben Mather <bwhmather@bwhmather.com>
2015-01-20 13:56:26 +0000

    reduce duplication in test loading

--- payment_terminal/tests/__init__.py
+++ payment_terminal/tests/__init__.py
@@ -1,19 +1,13 @@
 import unittest
 
 from payment_terminal.tests.test_loader import TestLoader
-from payment_terminal.tests.bbs import (
-    TestBBSFields, TestBBSMessages,
-    TestBBSTerminal, TestBBSConnection,
-    TestBBSPaymentSession,
-)
+import payment_terminal.tests.bbs
 
 
-loader = unittest.TestLoader()
-suite = unittest.TestSuite((
-    loader.loadTestsFromTestCase(TestLoader),
-    loader.loadTestsFromTestCase(TestBBSFields),
-    loader.loadTestsFromTestCase(TestBBSMessages),
-    loader.loadTestsFromTestCase(TestBBSTerminal),
-    loader.loadTestsFromTestCase(TestBBSConnection),
-    loader.loadTestsFromTestCase(TestBBSPaymentSession),
-))
+def suite():
+    loader = unittest.TestLoader()
+    suite = unittest.TestSuite((
+        loader.loadTestsFromModule(payment_terminal.tests.bbs),
+        loader.loadTestsFromTestCase(TestLoader),
+    ))
+    return suite
