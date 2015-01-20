091e250d245e381409aab331adddd6790f735d8b
Ben Mather <bwhmather@bwhmather.com>
2015-01-20 14:05:08 +0000

    relative imports

--- payment_terminal/drivers/bbs/tests/__init__.py
+++ payment_terminal/drivers/bbs/tests/__init__.py
@@ -1,12 +1,9 @@
-from payment_terminal.drivers.bbs.tests.test_fields import TestBBSFields
-from payment_terminal.drivers.bbs.tests.test_messages import TestBBSMessages
-from payment_terminal.drivers.bbs.tests.test_terminal import TestBBSTerminal
-from payment_terminal.drivers.bbs.tests.test_connection import (
-    TestBBSConnection,
-)
-from payment_terminal.drivers.bbs.tests.test_payment_session import (
-    TestBBSPaymentSession,
-)
+from .test_fields import TestBBSFields
+from .test_messages import TestBBSMessages
+from .test_terminal import TestBBSTerminal
+from .test_connection import TestBBSConnection
+from .test_payment_session import TestBBSPaymentSession
+
 
 __all__ = [
     'TestBBSFields', 'TestBBSMessages',
